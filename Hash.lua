fb4f756e59fee2a264973492dca3589d50d76573
rutheniumm <93807418+rutheniumm@users.noreply.github.com>
2024-10-10 15:47:04 -0400

    Create Hash.lua

---
+++ Hash.lua
@@ -0,0 +1,19 @@
+local Hash = {}
+local function jenkins_hash(key)
+    local hash = 0
+    for i = 1, #key do
+        hash = hash + string.byte(key, i)
+        hash = hash + bit32.lshift(hash, 10)
+        hash = bit32.bxor(hash, bit32.rshift(hash, 6))
+    end
+    hash = hash + bit32.lshift(hash, 3)
+    hash = bit32.bxor(hash, bit32.rshift(hash, 11))
+    hash = hash + bit32.lshift(hash, 15)
+    return hash
+end
+
+function Hash.generateSeedFromString(str, seed)
+    seed = seed or 0
+    return jenkins_hash(`{str}_{seed}`)
+end
+return Hash
